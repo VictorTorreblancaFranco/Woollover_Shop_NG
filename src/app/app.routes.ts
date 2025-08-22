10fe422952483784a4b32f036408feccfb7dc3f3
victor <victor.torreblanca.franco@vallegrande.edu.pe>
2025-08-22 01:07:40 -0500

    primera parte

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { Home } from './feature/home/home'; // Asegúrate de que la ruta esté bien importada
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    // otras rutas si es necesario
+];
--- src/app/feature/home/home.ts
+++ src/app/feature/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Carousel } from '../../feature/carousel/carousel';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Carousel, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
